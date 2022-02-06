e7b311f32a89a3db1746b775717dca32f000f0eb
be-prado <beprado@umich.edu>
2022-02-06 13:18:40 -0500

    fixing error

--- app.js
+++ app.js
@@ -2,17 +2,18 @@
 
 const switcher = document.querySelector('.btn');
 
-switcher.addEventListener('click', function() {
-    document.body.classList.toggle('dark-theme')
 
-    var className = document.body.className;
-    if(className == "light-theme") {
-        this.textContent = "Dark";
-    }
-    else {
-        this.textContent = "Light";
-    }
+//switcher.addEventListener('click', function() {
+//    document.body.classList.toggle('dark-theme')
 
-    console.log('current class name: ' + className);
+//    var className = document.body.className;
+//    if(className == "light-theme") {
+//        this.textContent = "Dark";
+//    }
+//    else {
+//        this.textContent = "Light";
+//    }
 
-});
+//    console.log('current class name: ' + className);
+
+//});
